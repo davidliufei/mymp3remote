a2d44b7bbbeac1696c7d990ae199f3c81b0ee0cc
david_liufei <david_liufei@163.com>
2020-11-01 12:42:40 +0800

    manager obj files in one file

--- common.mk
+++ common.mk
@@ -2,13 +2,20 @@
 SRCS = $(wildcard *.c)   #scan all source files
 OBJS = $(SRCS:.c=.o)     #replace all source files from .c to .o
 DEPS = $(SRCS:.c=.d)
+BIN := $(addprefix /home/david/share/mp3/, $(BIN))
+LINK_OBJ_DIR = /home/david/share/mp3/app/link_obj
+$(shell mkdir -p $(LINK_OBJ_DIR))
+OBJS := $(addprefix $(LINK_OBJ_DIR)/, $(OBJS))  #add prefix
+LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
+LINK_OBJ += $(OBJS)
 all: $(BIN) $(OBJS) $(DEPS)
 ifneq ("$(wildcard $(DEPS))", "")
 include $(DEPS)
 endif 
-$(BIN):$(OBJS)
-	gcc -o $@  $^  ../lcd/lcd.o  ../usb/usb.o  ../media/media.o
-%.o:%.c
+$(BIN):$(LINK_OBJ)
+	echo "LINK_OBJ=$(LINK_OBJ)"
+	gcc -o $@  $^
+$(LINK_OBJ_DIR)/%.o:%.c
 	gcc -o $@ -c $(filter %.c, $^)
 %.d:%.c
 	gcc -MM $^ > $@
--- makefile
+++ makefile
@@ -6,9 +6,11 @@ all:
 	make -C usb
 	make -C app
 clean:
-	rm -fr app/player.o app/player.d app/mp3
+	rm -fr mp3
+	rm -fr app/link_obj/*.o app/player.o app/player.d
 	rm -fr usb/usb.o  usb/usb.d
 	rm -fr lcd/lcd.o  lcd/lcd.d
 	rm -fr media/media.o  media/media.d
+	rm -fr app/link_obj
 		
 
