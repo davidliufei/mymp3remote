5842e9576a11140631fe9828c9288d68c9ee0fd8
david_liufei <david_liufei@163.com>
2020-11-15 01:19:26 +0800

    add support static lib

--- common.mk
+++ common.mk
@@ -5,21 +5,33 @@ DEPS = $(SRCS:.c=.d)
 BIN := $(addprefix $(BUILD_ROOT)/, $(BIN))
 LINK_OBJ_DIR = $(BUILD_ROOT)/app/link_obj
 $(shell mkdir -p $(LINK_OBJ_DIR))
-OBJS := $(addprefix $(LINK_OBJ_DIR)/, $(OBJS))  #add prefix
-LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
-LINK_OBJ += $(OBJS)
 DEP_DIR = $(BUILD_ROOT)/app/dep
 $(shell mkdir -p $(DEP_DIR))
+LIB_OBJ_DIR = $(BUILD_ROOT)/app/lib_obj
+$(shell mkdir -p $(LIB_OBJ_DIR))
+LIB_DIR  = $(BUILD_ROOT)/lib
+OBJ_DIR = $(LINK_OBJ_DIR)
+ifneq ("$(LIB)", "")
+OBJ_DIR = $(LIB_OBJ_DIR)
+endif
+OBJS := $(addprefix $(OBJ_DIR)/, $(OBJS))  #add prefix
 DEPS := $(addprefix $(DEP_DIR)/, $(DEPS))
-all: $(BIN) $(OBJS) $(DEPS)
+LIB := $(addprefix $(LIB_DIR)/, $(LIB))
+LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
+LINK_OBJ += $(OBJS)
+LIB_DEP = $(wildcard $(LIB_DIR)/*.a)
+LINK_LIB_NAME = $(patsubst lib%, -l%, $(basename $(notdir $(LIB_DEP))))
+all: $(BIN) $(OBJS) $(LIB) $(DEPS)
 ifneq ("$(wildcard $(DEPS))", "")
 include $(DEPS)
 endif 
-$(BIN):$(LINK_OBJ)
-	gcc -o $@  $^
-$(LINK_OBJ_DIR)/%.o:%.c
+$(BIN):$(LINK_OBJ) $(LIB_DEP)
+	gcc -o $@  $^ -L$(LIB_DIR) $(LINK_LIB_NAME)
+$(LIB):$(OBJS)
+	ar rcs $@ $^
+$(OBJ_DIR)/%.o:%.c
 	gcc -I$(HEAD_PATH) -o $@ -c $(filter %.c, $^)
 $(DEP_DIR)/%.d:%.c
-	gcc -I$(HEAD_PATH) -MM $^ | sed 's,\(.*\).o[ :]*, $(LINK_OBJ_DIR)/\1.o:,g' > $@
+	gcc -I$(HEAD_PATH) -MM $^ | sed 's,\(.*\).o[ :]*, $(OBJ_DIR)/\1.o:,g' > $@
 clean:
 	rm -f $(BIN) $(OBJS) $(DEPS)
